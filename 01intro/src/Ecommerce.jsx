532a93f7e18bb4fdd19aa98cc89875cddf2c411d
AnkushSharma74 <ankush11june@gmail.com>
2026-05-02 18:57:26 +0530

    Adding Filter option to Ecommerce compo

--- 01intro/src/Ecommerce.jsx
+++ 01intro/src/Ecommerce.jsx
@@ -7,6 +7,7 @@ import ProductCard from "./ProductCard";
 function Ecommerce(){
     const [product, setProduct] = React.useState([]);
     const [addedProduct,setAddedProduct]=React.useState([]);
+    const [search, setSearch]=React.useState("");
     
     //best practice to keep the fetch function inside the useEffect hook
     // or else you will have to memozie the function in order to prevent the unecessary rerenders
@@ -22,19 +23,37 @@ function Ecommerce(){
     } ,[])
 
     function handleAdd(num){
-       let result= product.filter((e,i)=>i==num);
-       console.log(result);
-       setAddedProduct([...addedProduct,result]);
+         const selectedProduct = product[num];
+         setAddedProduct(prev => [...prev, selectedProduct]);
+        //let result= product.filter((e,i)=>i==num);
+        //setAddedProduct([...addedProduct,result]);
     }
 
     //console.log(product);
     //console.log(addedProduct);
+
+    const filteredProducts = product.filter((item) =>
+        item.title.toLowerCase().includes(search.toLowerCase())
+        );
+        
+        console.log("filteredProducts");
+        console.log(filteredProducts);
 
     return(
 
         <div>
         <div className="flex  justify-between p-10 pt-10">
             <motion.div  initial={{opacity:0,x:100}} animate={{ opacity: 1,x:0}} className="">Ecommerce</motion.div>
+            {/* search functionality */}
+            <input
+                type="text"
+                placeholder="Search..."
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                className="border px-2 py-1 rounded"
+            />
+
+
             <div className="relative"><FaShoppingCart size={30} />
             <div className="absolute top-4 left-8">{addedProduct.length}</div>
             </div>
@@ -44,9 +63,9 @@ function Ecommerce(){
 
 
         <div className="flex  flex-wrap gap-10 justify-center">
-            {product.length>0?<>
+            {filteredProducts.length>0?<>
             
-            {product.map((e,i)=><ProductCard handleAdd={handleAdd} key={i} index={i} product={e}/>)}
+            {filteredProducts.map((e,i)=><ProductCard handleAdd={handleAdd} key={i} index={i} product={e}/>)}
             
             
             

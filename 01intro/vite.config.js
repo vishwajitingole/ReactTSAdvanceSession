acd9f04e4810130dd1178cc6d906869bcfa10e19
Vishwajeet Vikas Ingole <72921903+vishwajitingole@users.noreply.github.com>
2026-05-03 20:44:41 +0530

    Update vite.config.js

--- 01intro/vite.config.js
+++ 01intro/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+
+    ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173
+    },
+    build: {
+        outDir: 'dist'
+    },
+    base: '/'
+});

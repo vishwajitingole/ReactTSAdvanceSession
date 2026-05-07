4ee0c5927d979dab8d8d6d8dddd0e3c48fabae87
AnkushSharma74 <ankush11june@gmail.com>
2026-05-07 15:04:39 +0530

    Error Page changes

--- 01intro/src/Components/NotFound.jsx
+++ 01intro/src/Components/NotFound.jsx
@@ -1,12 +1,32 @@
+import { useNavigate } from "react-router";
+
+
 
 function NotFound() {
+
+  const navigate = useNavigate();
+
+
   return (
-    <div>
-      <p className="text-center">NotFound</p>
+    <div className="mt-10">
+      <p className="text-center">Not Found </p>
       <p className="text-center"> User will be redirected here, if there is no such path as entered by the user</p>
+
+      <div className="flex justify-center mt-2">
+        <button
+            onClick={() => navigate("/")}
+            className="bg-blue-600 text-black px-6 py-3 rounded-xl font-semibold shadow-lg hover:bg-gray-200 transition"
+          >
+            Go To Home
+        </button>
+      </div>
+
     </div>
+
+    
     
   )
 }
 
 export default NotFound
+
